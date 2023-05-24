9221cea727221b09e2a3844ef48c06549051c7ce
dothur <craz1000@naver.com>
2023-05-24 15:40:10 +0900

    feat & fix. UserDaoTest 로 분리, UserDao main 함수내용 옮기기 - 이렇게해서 UserDaoTest가 책임을 담당하도록 분리하였다. - 이제 서로 영향을 주지 않으면서도 필요에 따라 자유롭게 확장할 수 있는 구조가 되었다.

--- src/main/java/com/example/tobyspring3/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/dao/UserDao.java
@@ -42,19 +42,4 @@ public class UserDao {
 
         return user;
     }
-
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ConnectionMaker cm = new DConnectionMaker();
-        UserDao userDao = new UserDao(cm);
-        User user = new User();
-        user.setId("9");
-        user.setName("kyeongrok9");
-        user.setPassword("1232");
-        userDao.add(user);
-
-        User selectedUser = userDao.get("9");
-        System.out.println(selectedUser.getId());
-        System.out.println(selectedUser.getName());
-        System.out.println(selectedUser.getPassword());
-    }
 }
